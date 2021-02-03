21371f0922f92fe560f9e9b47d90d314a78c568e
Hatice <haticekaraty@gmail.com>
2021-02-02 22:45:34 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+
+class Song
+    attr_accessor :title, :artist
+
+    def initialize(title)
+        @title = title
+    end
+end
+
+#gulpembe = Song.new("Gulpembe")
+#gulpembe.title  => Gulpembe
+#gulpembe.artist = could be set to a artist baris manco or get the artist from artist class
+
+#song = Song.new("title")
+#song.title = artist
